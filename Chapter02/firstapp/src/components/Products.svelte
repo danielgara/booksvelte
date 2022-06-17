191bb6c4c06e901aa378e40e49bdcda56f0f55de
Daniel Correa <yo@danielgara.com>
2022-06-17 11:38:24 -0500

    imp: Chapter02

---
+++ Chapter02/firstapp/src/components/Products.svelte
@@ -0,0 +1,21 @@
+<script>
+    let products = [
+        {
+            id: "1",
+            name: "airpods"
+        },
+        {
+            id: "2",
+            name: "kindle"
+        },
+        {
+            id: "3",
+            name: "keyboard"
+        }                
+    ];
+</script>
+
+<h1>Products</h1>
+{#each products as product (product.id)}	
+    <h3>{product.id}: {product.name}</h3>
+{/each}
