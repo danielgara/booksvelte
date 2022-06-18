fc706cb8ae9e6b76de325c8264c4f575531bff47
Daniel Correa <yo@danielgara.com>
2022-06-18 09:18:51 -0500

    imp: Chapter08

---
+++ Chapter08/SvelteTodo/src/components/ToDoList.svelte
@@ -0,0 +1,48 @@
+<script>
+import {TodoStore} from '../stores';
+import { Table, Button } from 'sveltestrap';
+import {createEventDispatcher} from 'svelte';
+
+const dispatch = createEventDispatcher();
+
+const handleEdit = (todo) => {   
+    dispatch('edit-todo', todo)
+};
+
+const handleDelete = (todoId) => {
+    TodoStore.update(currentTodos => {
+	    return currentTodos.filter(todo => todo.id != todoId)    
+    });
+};
+</script>
+
+<Table>
+    <thead>
+      <tr>        
+        <th>To Do</th>
+        <th>Date Created</th>
+        <th>Complete</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody> 
+      {#each $TodoStore as todo (todo.id)}
+        <tr>
+          <td>{todo.text}</td>
+          <td>{todo.date}</td>
+          <td>Mark Complete</td>            
+          <td>
+            <Button on:click={() => handleEdit(todo)} color='warning'>
+                Edit
+            </Button>
+          </td>
+          <td>
+            <Button on:click={() => handleDelete(todo.id)} color='danger'>
+                Delete
+            </Button>    
+          </td>        
+        </tr>
+      {/each}                     
+    </tbody>
+</Table>
