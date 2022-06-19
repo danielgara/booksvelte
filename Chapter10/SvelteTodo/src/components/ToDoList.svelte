15fed1a548c9fa5ecbd7fdf9e30e9714673c4670
Daniel Correa <yo@danielgara.com>
2022-06-19 08:06:14 -0500

    imp: Chapter10

--- Chapter10/SvelteTodo/src/components/ToDoList.svelte
+++ Chapter10/SvelteTodo/src/components/ToDoList.svelte
@@ -29,6 +29,30 @@ const handleDelete = async (todoId) => {
 	    return currentTodos.filter(todo => todo.id != todoId)    
     });
 };
+
+const toggleComplete = async (todo) => {   
+  todo.complete = !todo.complete;
+  todo.dateCompleted = new Date().toDateString();
+
+  await axios.patch(endpoint+todo.id,{
+    complete:todo.complete, 
+    dateCompleted: todo.dateCompleted
+  });
+
+  TodoStore.update(currentTodos => {
+    const updatedToDoIndex = currentTodos.findIndex(
+      t => t.id === todo.id
+    );
+
+    const updatedToDos = [
+      ...currentTodos.slice(0,updatedToDoIndex),
+      todo,
+      ...currentTodos.slice(updatedToDoIndex + 1)
+    ];
+
+    return updatedToDos;
+  });    
+};
 </script>
 
 <Table>
@@ -43,21 +67,56 @@ const handleDelete = async (todoId) => {
     </thead>
     <tbody> 
       {#each $TodoStore as todo (todo.id)}
-        <tr>
-          <td>{todo.text}</td>
-          <td>{todo.date}</td>
-          <td>Mark Complete</td>            
-          <td>
-            <Button on:click={() => handleEdit(todo)} color='warning'>
-                Edit
-            </Button>
-          </td>
-          <td>
-            <Button on:click={() => handleDelete(todo.id)} color='danger'>
-                Delete
-            </Button>    
-          </td>        
-        </tr>
+        {#if !todo.complete}
+          <tr>
+            <td>{todo.text}</td>
+            <td>{todo.date}</td>
+            <td>
+              <Button on:click={() => toggleComplete(todo)} color='success'>
+                Mark Complete
+              </Button>  
+            </td>            
+            <td>
+              <Button on:click={() => handleEdit(todo)} color='warning'>
+                  Edit
+              </Button>
+            </td>
+            <td>
+              <Button on:click={() => handleDelete(todo.id)} color='danger'>
+                  Delete
+              </Button>    
+            </td>        
+          </tr>
+        {/if}
       {/each}                     
     </tbody>
+</Table>
+
+<br>
+<h1>Completed Todos</h1>
+<Table>
+  <thead>
+    <tr>        
+      <th>To Do</th>
+      <th>Date Completed</th>
+      <th>Complete</th>        
+      <th>Delete</th>
+    </tr>
+  </thead>
+  <tbody>      
+    {#each $TodoStore as todo (todo.id)}
+      {#if todo.complete}
+        <tr>            
+          <td>{todo.text}</td>
+          <td>{todo.dateCompleted}</td>
+          <td>
+            <Button on:click={() => toggleComplete(todo)}>
+              Mark Uncomplete                  
+            </Button>
+          </td>              
+          <td><Button on:click={() => handleDelete(todo.id)} color='danger'>Delete</Button></td>                                 
+        </tr>             
+      {/if} 
+    {/each}  
+  </tbody>
 </Table>
