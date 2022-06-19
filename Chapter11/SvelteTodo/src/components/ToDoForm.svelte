b0f827f357d892dcfddc121ec0b41e339489aa1c
Daniel Correa <yo@danielgara.com>
2022-06-19 08:07:18 -0500

    imp: pre-chapter11

---
+++ Chapter11/SvelteTodo/src/components/ToDoForm.svelte
@@ -0,0 +1,58 @@
+<script>
+import {v4 as uuidv4} from 'uuid';
+import { TodoStore } from '../stores';
+import {Form, FormGroup, Input, Alert, Button} from 'sveltestrap';
+import axios from 'axios';
+
+let text = '';    
+let textErrorMessage = '';
+let validText = false;
+const endpoint = "http://localhost:3000/todos/";
+
+const handleSubmit = async event => {
+    event.preventDefault();
+    handleInput();            
+    if(validText){
+        const newTodo = {
+            id: uuidv4(),
+            text: text,
+            complete:false,
+            date: new Date().toDateString(),
+            dateCompleted: false	                      
+        };
+
+        const response = await axios.post(endpoint,newTodo);
+                
+        TodoStore.update(currentTodos =>{
+            return [newTodo, ...currentTodos]
+        });
+        // to clear field after adding
+                
+        text = "";            
+        validText = false;            
+    }  
+};
+
+const handleInput = () => {
+    validText = false;
+    if(text.length < 10){
+        textErrorMessage = "Todo text should be minimum 10 characters";
+    }
+    else{
+        textErrorMessage = '';
+        validText = true;
+    }
+};
+</script>
+
+<Form on:submit={handleSubmit}>
+    <FormGroup floating label="Enter Todo Text">
+        <Input type="text" bind:value={text} />
+    </FormGroup>
+    {#if textErrorMessage.length > 0}
+        <Alert color="danger">            
+            {textErrorMessage}
+        </Alert>
+    {/if}    
+    <Button type="submit" color="primary">Add Todo</Button>   
+</Form>
---
+++ Chapter11/SvelteTodo/src/components/ToDoList.svelte
@@ -0,0 +1,122 @@
+<script>
+import {TodoStore} from '../stores';
+import { Table, Button } from 'sveltestrap';
+import {createEventDispatcher, onMount} from 'svelte';
+import axios from 'axios';
+
+const endpoint = "http://localhost:3000/todos/";
+
+onMount(async () => {
+  try {
+    const res = await axios.get(endpoint);
+    TodoStore.update(() => {
+      return res.data;
+    });
+  }
+  catch(e){}
+});
+
+const dispatch = createEventDispatcher();
+
+const handleEdit = (todo) => {   
+    dispatch('edit-todo', todo)
+};
+
+const handleDelete = async (todoId) => {
+    await axios.delete(endpoint + todoId);
+    
+    TodoStore.update(currentTodos => {
+	    return currentTodos.filter(todo => todo.id != todoId)    
+    });
+};
+
+const toggleComplete = async (todo) => {   
+  todo.complete = !todo.complete;
+  todo.dateCompleted = new Date().toDateString();
+
+  await axios.patch(endpoint+todo.id,{
+    complete:todo.complete, 
+    dateCompleted: todo.dateCompleted
+  });
+
+  TodoStore.update(currentTodos => {
+    const updatedToDoIndex = currentTodos.findIndex(
+      t => t.id === todo.id
+    );
+
+    const updatedToDos = [
+      ...currentTodos.slice(0,updatedToDoIndex),
+      todo,
+      ...currentTodos.slice(updatedToDoIndex + 1)
+    ];
+
+    return updatedToDos;
+  });    
+};
+</script>
+
+<Table>
+    <thead>
+      <tr>        
+        <th>To Do</th>
+        <th>Date Created</th>
+        <th>Complete</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody> 
+      {#each $TodoStore as todo (todo.id)}
+        {#if !todo.complete}
+          <tr>
+            <td>{todo.text}</td>
+            <td>{todo.date}</td>
+            <td>
+              <Button on:click={() => toggleComplete(todo)} color='success'>
+                Mark Complete
+              </Button>  
+            </td>            
+            <td>
+              <Button on:click={() => handleEdit(todo)} color='warning'>
+                  Edit
+              </Button>
+            </td>
+            <td>
+              <Button on:click={() => handleDelete(todo.id)} color='danger'>
+                  Delete
+              </Button>    
+            </td>        
+          </tr>
+        {/if}
+      {/each}                     
+    </tbody>
+</Table>
+
+<br>
+<h1>Completed Todos</h1>
+<Table>
+  <thead>
+    <tr>        
+      <th>To Do</th>
+      <th>Date Completed</th>
+      <th>Complete</th>        
+      <th>Delete</th>
+    </tr>
+  </thead>
+  <tbody>      
+    {#each $TodoStore as todo (todo.id)}
+      {#if todo.complete}
+        <tr>            
+          <td>{todo.text}</td>
+          <td>{todo.dateCompleted}</td>
+          <td>
+            <Button on:click={() => toggleComplete(todo)}>
+              Mark Uncomplete                  
+            </Button>
+          </td>              
+          <td><Button on:click={() => handleDelete(todo.id)} color='danger'>Delete</Button></td>                                 
+        </tr>             
+      {/if} 
+    {/each}  
+  </tbody>
+</Table>
